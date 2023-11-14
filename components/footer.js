c2b345e13a621d443cbe439808813a739e384013
Matthew Liebenberg <57996389+Matthew5704@users.noreply.github.com>
2023-11-14 12:16:11 +0200

    Initial commit

---
+++ components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    
+
+<footer className="fixed bottom-0 left-o w-full  dark:bg-gray-800 dark:border-gray-600 text-center">
+    <span className="">© 2023 <a href="https://britzfamily.co.za/" className="hover:underline">Estiaan Britz</a>, <a href="https://careerday.belgiumcampus.ac.za/sites/s578662/" className="hover:underline">Kwanda Ndaba</a>. All Rights Reserved.
+    </span>
+</footer>
+
+  )
+}
+
+export default footer;
---
+++ pages/infoquality.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const metadata = {
+  title: "INF181",
+  description: 'This an application that teaches CEOs how tech works'
+};
+
+const infoquality = () => {
+  return (
+    <Layout pageTitle={metadata.title} pageDescription={metadata.description}>
+    <div className="container mx-auto mt-8 p-4 bg-slate-700">
+      {/* Heading for Information Quality */}
+      <h1 className="text-2xl font-bold mb-4">
+        Information Quality:
+      </h1>
+
+      {/* Section 1: Definition of Information Quality */}
+      <section>
+        <p>
+          Information quality refers to how much value processed data can provide a user. The quality of the data is measured by several categories, mainly:
+        </p>
+        <ul className="list-disc ml-4">
+          <li>Completeness: The information provides all necessary details giving the user a better understanding of the topic.</li>
+          <li>Timeliness: Information is provided when requested, meaning that the information is up to date giving better insight into the current affairs of a given topic.</li>
+          <li>Validity: Refers to the degree to which information is accurate and truthful. Information with high validity aims to provide the most correct information which describes real-world entities.</li>
+          <li>Integrity: Refers to the degree to which information is accurate (correctness), consistent (elements of the information align uniformly), reliable (trustworthy), and complete.</li>
+          <li>Uniqueness: Refers to the accuracy in compiling distinct customer information such as a customer’s performance analytics related to a product or service you provide.</li>
+          <li>Consistency: Refers to the degree to which data is accurate, complete, and uniformly aligned throughout the dataset.</li>
+        </ul>
+      </section>
+
+      {/* Section 2: Challenges Without Information Quality */}
+      <section>
+        <p>
+          Without the assurance of accuracy and reliability in data quality, executives face challenges in trusting the data for making well-informed decisions. This lack of confidence can subsequently lead to increased operational costs and disrupt downstream users. Analysts find themselves relying on imperfect reports, drawing potentially misguided conclusions from these findings.
+        </p>
+        <p>
+          The flawed guidelines and practices that ensue can diminish the productivity of end-users. What’s more, poorly maintained data can give rise to various other issues. For instance, outdated customer information may cause missed opportunities for up- or cross-selling products and services. Low-quality data could result in a company shipping product to incorrect addresses, leading to decreased customer satisfaction ratings, a decline in repeat sales, and increased costs related to reshipments.
+        </p>
+        <p>
+          In industries subject to stringent regulations, the presence of bad data can expose the company to fines for improper financial or regulatory compliance reporting. This highlights the importance of Information Quality, with high quality providing a competitive advantage and low quality having the potential to ruin customer trust or the entire business.
+        </p>
+      </section>
+
+      {/* Section 3: Importance of Automation in Enhancing Data Quality */}
+      <section>
+        <p>
+          In the past, enhancing data quality through various initiatives was a costly and high-risk undertaking. Financial institutions, for instance, might have invested significant time and resources only to discover, at the end of the endeavor, that there were minimal tangible outcomes. However, with the advent of techniques and, more crucially, automation, it is now possible to consolidate all relevant candidate information inexpensively for processing and evaluation.
+        </p>
+        <p>
+          This enables rapid sorting of pertinent data from irrelevant details. Moreover, this automated process can be repeated as the world evolves, effortlessly adapting to changes in customer behavior and data availability. This represents a significant advantage offered by technology.
+        </p>
+      </section>
+
+      {/* Section 4: Technology's Role in Enhancing Data Quality */}
+      <section>
+        <p>
+          In summary, while technology can contribute significantly to enhancing data quality, it cannot fabricate information that doesn't exist. Similarly, it cannot augment the value of irrelevant information. Its primary strength lies not in cleverness but in consistency, automation, and a focus on diminishing risk, turnaround time, and the human effort required to manage the vast sea of data that surrounds us.
+        </p>
+      </section>
+
+    </div>
+    </Layout>
+  );
+};
+
+// Exporting the 'infoquality' component as the default export
+export default infoquality;
